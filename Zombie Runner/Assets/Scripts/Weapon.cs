afc032e0e1064bc5838badc40360ecc920632f8b
Nyppp <wlsdnrla216@naver.com>
2022-10-27 16:12:27 +0900

    총기 발사 이펙트 추가

--- Zombie Runner/Assets/Scripts/Weapon.cs
+++ Zombie Runner/Assets/Scripts/Weapon.cs
@@ -9,6 +9,10 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 50f;
 
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect; //Instantiate를 위해 게임오브젝트로 선언
+    
+
     void Update()
     {
         if(Input.GetButtonDown("Fire1"))
@@ -19,18 +23,28 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
 
-        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
-        { 
-            Debug.Log(hit.transform.name);
-
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
             // TODO : 히트 이펙트, 총알 맞는 사운드 추가
+            CreateHitImpact(hit);
 
             // 적이 총에 맞은 경우 체력감소
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
 
-            if(target != null)
+            if (target != null)
             {
                 target.TakeDamage(damage);
             }
@@ -39,5 +53,11 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject Impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(Impact, 0.1f);
     }
 }
