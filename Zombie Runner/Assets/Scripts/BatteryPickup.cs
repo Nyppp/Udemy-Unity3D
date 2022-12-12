802818e0a524564fc17e5ef005df7f5bab9a9da0
Nyppp <wlsdnrla216@naver.com>
2022-12-12 16:49:38 +0900

    프로빌더 머터리얼 적용, 손전등 기능 및 라이팅 추가

---
+++ Zombie Runner/Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    [SerializeField] float batteryAmount = 50f;
+    [SerializeField] float angleAmount = 30f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<FlashLightSystem>().RestoreLightIntensity(batteryAmount);
+            other.gameObject.GetComponent<FlashLightSystem>().RestoreLightAngle(angleAmount);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Zombie Runner/Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minAngle = 40f;
+
+    Light playerLight;
+
+    private void Start()
+    {
+        playerLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float reastoreAngle)
+    {
+        playerLight.spotAngle = reastoreAngle;
+    }
+
+    public void RestoreLightIntensity(float intensityAmount)
+    {
+        playerLight.intensity += intensityAmount;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        playerLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (playerLight.spotAngle <= minAngle)
+        {
+            return;
+        }
+        else
+        {
+            playerLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+}
